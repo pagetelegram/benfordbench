886be155de014488e36fc400b86e87a85dfd1f97
Page Telegram <43476661+pagetelegram@users.noreply.github.com>
2020-12-22 17:46:55 -0600

    Update method.sh

--- method.sh
+++ method.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 
-echo Benford Bench: Time Stamps of Election Logs Chicago Board of Elections...
-echo method 'keyword' 'electionday'
+echo Benford Bench: Delta Time Stamps of Election Logs Board of Elections
+echo For rendering with bxe [benfordbenchXe]
+echo method [ballot keyword] [electionday keyword]
+echo press a key to continue or ctrl+"C" to escape
 
-echo Method:
+read p
+
+echo Processing Steps Commence:
 
 echo 1. Combine all precinct files to one Ward file.
 
 cat *.txt > all.tx
 
-echo 2. Extract all lines with key expression 'Ballot saved'
+echo 2. Extract all lines with key expression [ballot keyword]
 
 cat all.tx | grep $1 > keyword.tx
 
@@ -26,9 +30,9 @@ echo 5. Convert time stamps to total seconds
 
 cat timestamps.tx | nawk -F: '{ seconds=($1*60)*60; seconds=seconds+($2*60); seconds=seconds+$3; print seconds}' > seconds.tx
 
-echo 6. Sort seconds ascending
+echo Skipping 6. Sort seconds ascending
 
-sort -n seconds.tx > all.dat
+mv seconds.tx all.dat
 
 echo 7. Count total number of ballots saved
 
@@ -37,9 +41,14 @@ tail counted.tx
 
 mv timestamps.tx allts.dat
 
-echo 8. Run BenfordXe per Ward. Source code for bxe.bas available at https://github.com/pagetelegram/benfordbench
+echo 8. Making delta seconds file...
 
+awk 'p{print $0-p}{p=$0}' all.dat > dall.dat
+
+tail dall.dat
+
+echo 9. Run BenfordXe per Ward. Source code for bxe.bas available at https://github.com/pagetelegram/benfordbench
 echo ie
-
 echo bxe all.dat   [for total seconds]
 echo bxe allts.dat [for actual time stamps]
+echo bxe dall.dat [for delta seconds]
